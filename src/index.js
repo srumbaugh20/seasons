389a16974e271049e2bc7a191f6d00e250415321
Seth Rumbaugh <sethrumbaugh@gmail.com>
2021-01-25 14:11:11 -0700

    new repo

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    render() {
+        return( <div>
+            Latitude: {this.state.lat}
+            <br />
+            Error: {this.state.errorMessage}
+        </div>
+    )};
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+);
